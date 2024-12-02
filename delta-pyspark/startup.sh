171feec948038f7dfa6e4019e9497bceb0031bac
MadMax55555 <saadibella04@gmail.com>
2024-12-02 21:06:29 +0100

    pysparked

--- delta-pyspark/startup.sh
+++ delta-pyspark/startup.sh
@@ -10,4 +10,5 @@ export DELTA_PACKAGE_VERSION=delta-spark_2.12:${DELTA_SPARK_VERSION}
 $SPARK_HOME/bin/pyspark --packages io.delta:${DELTA_PACKAGE_VERSION} \
   --conf "spark.driver.extraJavaOptions=-Divy.cache.dir=/tmp -Divy.home=/tmp -Dio.netty.tryReflectionSetAccessible=true" \
   --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
-  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+  --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
+  --conf "spark.ui.host=0.0.0.0"
